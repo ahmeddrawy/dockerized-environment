c3c065ac4287cdee74d8e64931bb44652f160cfd
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-03-03 20:28:08 +0200

    use postgres,connect to db

--- API/index.js
+++ API/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
-const HOST = '0.0.0.0'
+const HOST = '0.0.0.0';
+const {Client} = require('pg');
+const client  = new Client();
+console.log(client);
+client.connect().then(()=>{
+    console.log('connected to db');
+}).catch(()=>{
+    console.log("failed to connect");
+})
+
 // app.get('/', (req, res) => res.send('Halan ROCKS'))
 app.get('/',  (req, res) =>{
     let n =req.query.n;
     if(!n)
-        res.send('HALAN ROCKS')
+        res.send('HALAN ROCKsss')
     else{
         let ret= n*n ;
         res.send(ret.toString());
